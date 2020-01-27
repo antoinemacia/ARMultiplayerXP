c1a3cf7a357bdc6ebc59c3086eff55a88d93aa6f
Antoine Macia <antoine.macia83@gmail.com>
2020-01-28 08:51:03 +1100

    Add joystick and script to control it

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public Joystick joystick;
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // Taking the joystick inputs
+      float _xMovementInput = joystick.Horizontal;
+      float _zMovementInput = joystick.Vertical;
+
+      // Calculating velocity vectors
+      Vector3 _MovementHorizontal = transform.right * _xMovementInput;
+      Vector3 _MovementVertical = transform.forward * _zMovementInput;
+
+      // Calculate the final movement velocity vector
+      Vector3 _MovementVelocityVector = (_MovementHorizontal + _MovementVertical).normalized * speed;
+
+      // Apply Movement
+    }
+}
