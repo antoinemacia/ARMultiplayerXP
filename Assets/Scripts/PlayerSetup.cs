91d691860d49bf8095b326cad04c7d7360f94a3e
Antoine Macia <antoine.macia83@gmail.com>
2020-02-14 08:18:32 +1100

    Add PlayerSetup script to differentiate local from remote player

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+// ----------------------------------------------------------------
+// This script ensures the setup of the game is for local gaming only
+// ----------------------------------------------------------------
+public class PlayerSetup : MonoBehaviourPun {
+  // Start is called before the first frame update
+  void Start () {
+    if (photonView.IsMine) {
+      // This means this player is my player
+      // NOTE - the bellow object can be accessed this way since it lives on the same GameObject
+      transform.GetComponent<MovementController> ().enabled = true;
+      transform.GetComponent<MovementController> ().joystick.gameObject.SetActive (true);
+    } else {
+      // Player is other players (remote)
+      transform.GetComponent<MovementController> ().enabled = false;
+      transform.GetComponent<MovementController> ().joystick.gameObject.SetActive (false);
+    }
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+}
