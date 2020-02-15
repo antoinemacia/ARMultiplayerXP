83027dc15a7406eb2bfd33b01896a66fce5b0140
Antoine Macia <antoine.macia83@gmail.com>
2020-02-15 12:04:13 +1100

    Implement synchronisation script for multiplayer

---
+++ Assets/Scripts/MySynchonisationScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class MySynchonisationScript : MonoBehaviour, IPunObservable {
+
+  RigidBody rb;
+  PhotonView photonView;
+  Vector3 newtorkPosition;
+  Quaternion networkRotation;
+
+  // This is loaded before the script start (kinda like an initializer)
+  private void Awake () {
+    rb = GetComponent<RigidBody> ();
+    photonView = GetComponent<PhotonView> ();
+  }
+
+  // Start is called before the first frame update
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+
+  // Here using FixedUodate since we're dealing with a RigidBody
+  private void FixedUpdate () {
+
+    if (!photonView.IsMine) {
+      rb.position = Vector3.MoveTowards (rb.position, networkPosition, Time.fixedDeltaTime);
+      rb.rotation = Quaternion.RotateTowards (rb.rotation, networkRotation, Time.fixedDeltaTime * 100);
+    }
+  }
+
+  public OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info) {
+    // This stream is sending and receiving data to a PhotonView observable
+    if (stream.IsWriting) {
+      // If true, this means the photon view is running on my device and I'm the one to who
+      // Controls this player. We should send our position & rotation data to the the "REMOTE ME"
+      // Which is the version of my player on other peoples devices
+      stream.SendNext (rb.position);
+      stream.SendNext (rb.rotation);
+    } else {
+      // If false, it means the stream is reading. Meaning we're listening to other players.
+      networkPosition = (Vector3) stream.ReceiveNext ();
+      networkRotation = (Quartenion) stream.ReceiveNext ();
+    }
+  }
+}
