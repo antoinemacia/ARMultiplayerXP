a164fcfd23c1550894636e7a58de07919fbf79bc
Antoine Macia <antoine.macia83@gmail.com>
2020-02-21 08:25:50 +1100

    Create Battle script

---
+++ Assets/Scripts/BattleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleScript : MonoBehaviour {
+
+  Rigidbody deviceRigidBody;
+
+  // This is loaded before the script start (kinda like an initializer)
+  private void Awake () {
+    deviceRigidBody = GetComponent<Rigidbody> ();
+  }
+
+  void OnCollisionEnter (Collision collision) {
+    if (collision.rigidbody) {
+      Rigidbody opponent = collision.rigidbody;
+
+      if (opponent.velocity.magnitude > deviceRigidBody.velocity.magnitude) {
+        Debug.Log ("Collision!!!!");
+      }
+    }
+
+  }
+}
