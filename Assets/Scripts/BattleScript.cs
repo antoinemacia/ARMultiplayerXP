0ffc66ffc189362311c840954ce1a61f121fbd47
Antoine Macia <antoine.macia83@gmail.com>
2020-02-23 14:41:50 +1100

    Add damage reduction logic in Battle script

--- Assets/Scripts/BattleScript.cs
+++ Assets/Scripts/BattleScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -30,6 +31,7 @@ public class BattleScript : MonoBehaviour {
     if (collision.gameObject.CompareTag ("Player")) {
       Rigidbody opponent = collision.collider.gameObject.GetComponent<Rigidbody> ();
 
+      // Compare the speed of both players, the fastest makes the gamages
       if (opponent.velocity.magnitude > deviceRigidBody.velocity.magnitude) {
         float mySpeed = deviceRigidBody.velocity.magnitude;
         float opponentSpeed = opponent.velocity.magnitude;
@@ -37,12 +39,25 @@ public class BattleScript : MonoBehaviour {
         Debug.Log ("My Speed:" + mySpeed + "----- Opponent Speed:" + opponentSpeed);
 
         if (mySpeed > opponentSpeed) {
-          Debug.Log ("You dameged other player");
+          // Apply damage to the slower player
+          if (collision.collider.gameObject.GetComponent<PhotonView> ().IsMine) {
+            float damagePerHit = 400f;
+            // Here we use RPC calls so thatn they're broadcasted to all users
+            collision.collider.gameObject.GetComponent<PhotonView> ().RPC ("DoDamage", RpcTarget.AllBuffered, damagePerHit);
+          }
         } else {
           Debug.Log ("You got damaged!!");
         }
       }
     }
+  }
 
+  [PunRPC]
+  public void DoDamage (float damageAmount) {
+    spinnerScript.spinSpeed -= damageAmount;
+    currentSpinSpeed = spinnerScript.spinSpeed;
+
+    spinSpeedBar_Image.fillAmount = currentSpinSpeed / startSpinSpeed;
+    spinSpeedBar_Text.text = currentSpinSpeed + "/" + startSpinSpeed;
   }
 }
