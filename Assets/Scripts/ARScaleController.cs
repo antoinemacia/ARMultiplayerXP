0c6a7f97cc2ccafdc7afe003e55d78ce209b282b
Antoine Macia <antoine.macia83@gmail.com>
2020-02-27 08:53:49 +1100

    Added Scale controller script

---
+++ Assets/Scripts/ARScaleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class ARScaleController : MonoBehaviour {
+
+  ARSessionOrigin m_ARSessionOrigin;
+  public Slider scaleSlider;
+
+  private void Awake () {
+    m_ARSessionOrigin = GetComponent<ARSessionOrigin> ();
+  }
+
+  // Start is called before the first frame update
+  void Start () {
+    scaleSlider.onValueChanged.AddListener (OnSliderValueChanged);
+  }
+
+  // Update is called once per frame
+  void Update () { }
+
+  public void OnSliderValueChanged (float value) {
+    if (scaleSlider != null) {
+      m_ARSessionOrigin.transform.localScale = Vector3.one * value;
+    }
+  }
+}
